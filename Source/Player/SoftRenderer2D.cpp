c70cd46f62dd88082fc13f1978e317286e3aab10
ideugu <ideugu@ck.ac.kr>
2020-09-10 10:30:51 +0900

    방정식으로 점 생성하기

--- Source/Player/SoftRenderer2D.cpp
+++ Source/Player/SoftRenderer2D.cpp
@@ -49,30 +49,42 @@ void SoftRenderer::Update2D(float InDeltaSeconds)
 	_CurrentColor = input.SpacePressed() ? LinearColor::Red : LinearColor::Blue;
 }
 
-static std::vector<Vector2> hearts = { Vector2(0.000f, 109.141f),Vector2(0.000f, -109.141f),Vector2(69.421f, 123.000f),Vector2(-69.421f, 123.000f),Vector2(46.344f, 123.000f),Vector2(-46.344f, 123.000f),Vector2(96.000f, 114.000f),Vector2(-96.000f, 114.000f),Vector2(11.294f, 114.000f),Vector2(-11.294f, 114.000f),Vector2(107.103f, 105.000f),Vector2(-107.103f, 105.000f),Vector2(114.000f, 96.000f),Vector2(-114.000f, 96.000f),Vector2(118.523f, 87.000f),Vector2(-118.523f, 87.000f),Vector2(121.412f, 78.000f),Vector2(-121.412f, 78.000f),Vector2(123.053f, 69.000f),Vector2(-123.053f, 69.000f),Vector2(123.676f, 60.000f),Vector2(-123.676f, 60.000f),Vector2(123.432f, 51.000f),Vector2(-123.432f, 51.000f),Vector2(122.421f, 42.000f),Vector2(-122.421f, 42.000f),Vector2(120.715f, 33.000f),Vector2(-120.715f, 33.000f),Vector2(118.361f, 24.000f),Vector2(-118.361f, 24.000f),Vector2(115.394f, 15.000f),Vector2(-115.394f, 15.000f),Vector2(111.836f, 6.000f),Vector2(-111.836f, 6.000f),Vector2(107.697f, -3.000f),Vector2(-107.697f, -3.000f),Vector2(102.979f, -12.000f),Vector2(-102.979f, -12.000f),Vector2(97.674f, -21.000f),Vector2(-97.674f, -21.000f),Vector2(91.765f, -30.000f),Vector2(-91.765f, -30.000f),Vector2(85.221f, -39.000f),Vector2(-85.221f, -39.000f),Vector2(78.000f, -48.000f),Vector2(-78.000f, -48.000f),Vector2(70.039f, -57.000f),Vector2(-70.039f, -57.000f),Vector2(61.247f, -66.000f),Vector2(-61.247f, -66.000f),Vector2(51.496f, -75.000f),Vector2(-51.496f, -75.000f),Vector2(40.585f, -84.000f),Vector2(-40.585f, -84.000f),Vector2(28.194f, -93.000f),Vector2(-28.194f, -93.000f),Vector2(13.740f, -102.000f),Vector2(-13.740f, -102.000f) };
-
 // 렌더링 로직
 void SoftRenderer::Render2D()
 {
 	// 격자 그리기
 	DrawGrid2D();
 
-	for (auto const& v : hearts)
+	static std::vector<Vector2> hearts;
+	if (hearts.empty())
 	{
-		Vector2 target = v + _CurrentPosition;
-
-		// 지정한 점을 기준으로 상하좌우로 점 찍기
-		_RSI->DrawPoint(target, _CurrentColor);
-		_RSI->DrawPoint(target + Vector2::UnitX, _CurrentColor);
-		_RSI->DrawPoint(target - Vector2::UnitX, _CurrentColor);
-		_RSI->DrawPoint(target + Vector2::UnitY, _CurrentColor);
-		_RSI->DrawPoint(target - Vector2::UnitY, _CurrentColor);
+		static float range = 10.f;
+		static float increment = 0.05f;
+		static float n = 10.f;
+		static float tolerance = 0.5f;
+		for (float y = -range; y < range; y += increment)
+		{
+			for (float x = -range; x < range; x += increment)
+			{
+				float test = x * x * y * y * y;
+				float u = (x * x + y * y - (n - tolerance));
+				float l = (x * x + y * y - (n + tolerance));
+				float utest = u * u * u;
+				float ltest = l * l * l;
+				if(utest > test && ltest < test)
+				{
+					hearts.push_back(Vector2(x, y));
+				}
+			}
+		}
 	}
 
-	Vector2 v = ScreenPoint(400, 300).ToVectorCoordinate(_ScreenSize);
-	_RSI->DrawPoint(v, _CurrentColor);
-	ScreenPoint p = ScreenPoint::ToScreenCoordinate(_ScreenSize, Vector2(0.9f, 0.1f));
-	ScreenPoint p1 = ScreenPoint::ToScreenCoordinate(_ScreenSize, Vector2(0.9f, 0.f));
+	for (auto const& v : hearts)
+	{
+		static float scale = 20.f;
+		Vector2 target = (v * scale) + _CurrentPosition;
+		_RSI->DrawPoint(target, _CurrentColor);
+	}
 
 	// 현재 위치를 화면에 출력
 	_RSI->PushStatisticText(_CurrentPosition.ToString());
