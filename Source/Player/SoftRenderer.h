27bf2950df239a1674b2b2e52110ef44c34a334d
ideugu <guru@dustinlee.me>
2020-09-13 17:35:37 +0900

    행렬 곱

--- Source/Player/SoftRenderer.h
+++ Source/Player/SoftRenderer.h
@@ -50,9 +50,9 @@ private:
 	int _Grid2DUnit = 10;
 
 	// 로직 구현을 위한 멤버 변수
-	float _CurrentScale = 10.f;
+	float _CurrentShear = 0.f;
+	float _CurrentScale = 5.f;
 	float _CurrentDegree = 0.f;
-	Vector2 _CurrentPosition = Vector2::Zero;
 
 private:
 	// 초기화 점검 변수
--- Source/Player/SoftRenderer2D.cpp
+++ Source/Player/SoftRenderer2D.cpp
@@ -39,15 +39,18 @@ void SoftRenderer::DrawGrid2D()
 void SoftRenderer::Update2D(float InDeltaSeconds)
 {
 	// 게임 로직에만 사용하는 변수
-	static float moveSpeed = 100.f;
+	static float shearSpeed = 2.f;
+	static float scaleSpeed = 10.f;
 	static float rotateSpeed = 180.f;
 
 	// 엔진 모듈에서 입력 관리자 가져오기
 	InputManager input = _GameEngine.GetInputManager();
+	float deltaShear = input.GetXAxis() * shearSpeed * InDeltaSeconds;
+	float deltaScale = input.GetZAxis() * scaleSpeed * InDeltaSeconds;
 	float deltaRotation = input.GetWAxis() * rotateSpeed * InDeltaSeconds;
-	Vector2 deltaPosition = Vector2(input.GetXAxis(), input.GetYAxis()) * moveSpeed * InDeltaSeconds;
 
-	_CurrentPosition += deltaPosition;
+	_CurrentShear += deltaShear;
+	_CurrentScale = Math::Clamp(_CurrentScale + deltaScale, 5.f, 15.f);
 	_CurrentDegree += deltaRotation;
 }
 
@@ -75,35 +78,59 @@ void SoftRenderer::Render2D()
 		}
 	}
 
+	static const Vector2 pivot(200.f, 0.f);
+
+	// 크기 행렬
+	Vector2 sBasis1(_CurrentScale, 0.f);
+	Vector2 sBasis2(0.f, _CurrentScale);
+	Matrix2x2 sMat(sBasis1, sBasis2);
+
+	// 회전 행렬
+	float sin, cos;
+	Math::GetSinCos(sin, cos, _CurrentDegree);
+	Vector2 rBasis1(cos, sin);
+	Vector2 rBasis2(-sin, cos);
+	Matrix2x2 rMat(rBasis1, rBasis2);
+
+	// 밀기 행렬
+	Vector2 shBasis1 = Vector2::UnitX;
+	Vector2 shBasis2(_CurrentShear, 1.f);
+	Matrix2x2 shMat(shBasis1, shBasis2);
+
+	// 합성 행렬
+	Matrix2x2 cMat = shMat * rMat * sMat;
+
 	// 각 값 초기화
 	rad = 0.f;
 	HSVColor hsv(0.f, 1.f, 0.85f); // 잘 보이도록 채도를 조금만 줄였음. 
+
+	// 행렬 연산 횟수 
+	int leftCnt = 0;
+	int rightCnt = 0;
 	for (auto const& v : hearts)
 	{
-		float sin, cos;
-		Math::GetSinCos(sin, cos, _CurrentDegree);
-		float halfScreenX = _ScreenSize.X * 0.5f;
-		float halfScreenY = _ScreenSize.Y * 0.5f;
-		Vector2 target = v * _CurrentScale;
-		Vector2 ndc = Vector2(target.X / halfScreenX, target.Y / halfScreenY);
-		float len = Vector2(ndc.X * _ScreenSize.Y / _ScreenSize.X, ndc.Y).Size();
-		Vector2 polarNdc = ndc.ToPolarCoordinate();
-		// 원점에서 멀어질 수록 더욱 회전을 부여함.
-		polarNdc.Y += Math::Deg2Rad(_CurrentDegree) * Math::Lerp(0.f, 1.f, len);
-		float angle = ndc.Angle() + Math::Deg2Rad(_CurrentDegree) * Math::Lerp(0.f, 1.f, len);
-		float radius = ndc.Size();
-		target = polarNdc.ToCartesianCoordinate();
-		target = Vector2(target.X * halfScreenX, target.Y * halfScreenY);
-		Vector2 target2 = Vector2(radius * cosf(angle) * (_ScreenSize.X * 0.5f), radius * sinf(angle) * (_ScreenSize.Y * 0.5f));
-
 		hsv.H = rad / Math::TwoPI;
-		_RSI->DrawPoint(target + _CurrentPosition, hsv.ToLinearColor());
+
+		// 왼쪽 하트 ( 행렬을 각각 곱하기 )
+		Vector2 left = sMat * v;
+		left = rMat * left;
+		left = shMat * left;
+		leftCnt += 3;
+		_RSI->DrawPoint(left - pivot, hsv.ToLinearColor());
+
+		// 오른쪽 하트 ( 행렬을 한 번만 곱하기 )
+		Vector2 right = cMat * v;
+		rightCnt++;
+		_RSI->DrawPoint(right + pivot, hsv.ToLinearColor());
+
 		rad += increment;
 	}
 
 	// 현재 위치와 스케일을 화면에 출력
-	_RSI->PushStatisticText(std::string("Position : ") + _CurrentPosition.ToString());
+	_RSI->PushStatisticText(std::string("Shear : ") + std::to_string(_CurrentShear));
 	_RSI->PushStatisticText(std::string("Scale : ") + std::to_string(_CurrentScale));
 	_RSI->PushStatisticText(std::string("Rotation : ") + std::to_string(_CurrentDegree));
+	_RSI->PushStatisticText(std::string("Count (Left) : ") + std::to_string(leftCnt));
+	_RSI->PushStatisticText(std::string("Count (Right) : ") + std::to_string(rightCnt));
 }
 
