bad5dffb77f4bc0c20870f54df6c9b5bdc890525
ideugu <ideugu@ck.ac.kr>
2020-09-17 10:15:49 +0900

    API 수정

--- Source/Runtime/Renderer/Public/RenderingSoftwareInterface.h
+++ Source/Runtime/Renderer/Public/RenderingSoftwareInterface.h
@@ -16,6 +16,7 @@ public:
 	virtual void EndFrame() = 0;
 
 	virtual void DrawPoint(const Vector2& InVectorPos, const LinearColor& InColor) = 0;
+	virtual void DrawPoint(const ScreenPoint& InScreenPos, const LinearColor& InColor) = 0;
 	virtual void DrawLine(const Vector2& InStartPos, const Vector2& InEndPos, const LinearColor& InColor) = 0;
 
 	virtual void DrawFullVerticalLine(int InX, const LinearColor& InColor) = 0;
